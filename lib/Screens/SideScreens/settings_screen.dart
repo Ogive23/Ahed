7dee9570ac21fdf6a7ab3cebae1b1a51c202cf43
Mahmoued Mohamed <mahmouedmartin222@yahoo.com>
2021-06-19 10:39:24 +0200

    Update screens to match theme

--- lib/Screens/SideScreens/settings_screen.dart
+++ lib/Screens/SideScreens/settings_screen.dart
@@ -1,12 +1,15 @@
+import 'package:ahed/Custom%20Widgets/CustomSpacing.dart';
 import 'package:ahed/Shared%20Data/app_language.dart';
 import 'package:ahed/Shared%20Data/app_theme.dart';
 import 'package:ahed/Shared%20Data/common_data.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
+import '../../GeneralInfo.dart';
 import '../../Session/session_manager.dart';
 
 class SettingsScreen extends StatelessWidget {
+  double w, h;
   final SessionManager sessionManager = new SessionManager();
   static CommonData commonData;
   static AppTheme appTheme;
@@ -16,129 +19,171 @@ class SettingsScreen extends StatelessWidget {
     commonData = Provider.of<CommonData>(context);
     appTheme = Provider.of<AppTheme>(context);
     appLanguage = Provider.of<AppLanguage>(context);
+    w = MediaQuery.of(context).size.width;
+    h = MediaQuery.of(context).size.height;
     return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0.0,
+        title: Padding(
+          padding: EdgeInsets.only(top: h / 20),
+          child: Text(
+            'الإعدادات',
+            style: appTheme.nonStaticGetTextStyle(
+                1.0,
+                Colors.black,
+                appTheme.getTextTheme(context) * 1.5,
+                FontWeight.w600,
+                1.0,
+                TextDecoration.none,
+                'Delius'),
+          ),
+        ),
+      ),
       body: Container(
-        alignment: Alignment.center,
-        padding: EdgeInsets.symmetric(
-            vertical: MediaQuery.of(context).size.height / 50),
-        decoration: BoxDecoration(color: appTheme.themeData.primaryColor),
-        child: SingleChildScrollView(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Directionality(
-                  textDirection: appLanguage.textDirection,
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Padding(
-                        padding: EdgeInsets.symmetric(
-                          horizontal: MediaQuery.of(context).size.width / 10,
-                        ),
-                        child: Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: <Widget>[
-                              Text(appLanguage.words['SettingsDarkMode'],
-                                  style: appTheme
-                                      .themeData.primaryTextTheme.subtitle1),
-                              Switch(
-                                value: appTheme.isDark,
-                                activeColor:
-                                    appTheme.themeData.toggleableActiveColor,
-                                onChanged: (value) {
-                                  sessionManager.createPreferredTheme(value);
-                                  appTheme.changeTheme(value);
-                                },
-                              ),
-                            ]),
+        height: double.infinity,
+        decoration: BoxDecoration(color: Colors.white),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            CustomSpacing(),
+            CustomSpacing(),
+            CustomSpacing(),
+            Directionality(
+                textDirection: appLanguage.textDirection,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.symmetric(
+                        horizontal: MediaQuery.of(context).size.width / 10,
                       ),
-                      Divider(),
-                      Padding(
-                        padding: EdgeInsets.symmetric(
-                          horizontal: MediaQuery.of(context).size.width / 10,
-                        ),
-                        child: Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: <Widget>[
-                              Text(appLanguage.words['SettingsLanguage'],
-                                  style: appTheme
-                                      .themeData.primaryTextTheme.subtitle1),
-                              DropdownButton(
-                                  items: [
-                                    DropdownMenuItem(
-                                      child: Text(
-                                        'العربية',
-                                        style: appTheme.themeData
-                                            .primaryTextTheme.bodyText2,
-                                      ),
-                                      value: 'Ar',
-                                    ),
-                                    DropdownMenuItem(
-                                      child: Text(
-                                        'En',
-                                        style: appTheme.themeData
-                                            .primaryTextTheme.bodyText2,
-                                      ),
-                                      value: 'En',
-                                    )
-                                  ],
-                                  value: appLanguage.language,
-                                  dropdownColor: Colors.amber,
-                                  icon: Icon(Icons.language),
-                                  // style: appTheme.themeData.textTheme.body1,
-                                  onChanged: (value) {
-                                    sessionManager
-                                        .createPreferredLanguage(value);
-                                    appLanguage.changeLanguage(value);
-                                  }),
-                            ]),
-                      ),
-                      Divider(),
-                      Padding(
-                        padding: EdgeInsets.symmetric(
-                          horizontal: MediaQuery.of(context).size.width / 10,
-                        ),
-                        child: Row(
+                      child: Row(
                           mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          //ToDo: Add Word to App Language for arabic support
-                          children: [
-                            Text('Legal Info',
+                          children: <Widget>[
+                            Text(appLanguage.words['SettingsDarkMode'],
                                 style: appTheme
                                     .themeData.primaryTextTheme.subtitle1),
-                            IconButton(
-                              icon: Icon(
-                                Icons.info,
-                                color: appTheme.themeData.iconTheme.color,
-                              ),
-                              onPressed: () {
-                                showDialog(
-                                  context: context,
-                                  builder: (context) {
-                                    return AboutDialog(
-                                      applicationName: 'BreedMe',
-                                      applicationVersion: '1.0.0',
-                                      children: <Widget>[
-                                        Text(
-                                            'Animations rights reserved to Lottie'),
-                                        Text(
-                                            'Fonts rights reserved to Google Fonts'),
-                                        Text('OGIVE ©${DateTime.now().year}')
-                                      ],
-                                    );
-                                  },
-                                );
+                            Switch(
+                              value: appTheme.isDark,
+                              activeColor:
+                                  appTheme.themeData.toggleableActiveColor,
+                              onChanged: (value) {
+                                sessionManager.createPreferredTheme(value);
+                                appTheme.changeTheme(value, context);
                               },
-                              tooltip: 'License',
                             ),
-                          ],
-                        ),
-                      )
-                    ],
-                  )),
-            ],
-          ),
+                          ]),
+                    ),
+                    Divider(),
+                    Padding(
+                      padding: EdgeInsets.symmetric(
+                        horizontal: MediaQuery.of(context).size.width / 10,
+                      ),
+                      child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: <Widget>[
+                            Text(appLanguage.words['SettingsLanguage'],
+                                style: appTheme
+                                    .themeData.primaryTextTheme.subtitle1),
+                            DropdownButton(
+                                items: [
+                                  DropdownMenuItem(
+                                    child: Text(
+                                      'العربية',
+                                      style: appTheme
+                                          .themeData.primaryTextTheme.bodyText2,
+                                    ),
+                                    value: 'Ar',
+                                  ),
+                                  DropdownMenuItem(
+                                    child: Text(
+                                      'En',
+                                      style: appTheme
+                                          .themeData.primaryTextTheme.bodyText2,
+                                    ),
+                                    value: 'En',
+                                  )
+                                ],
+                                value: appLanguage.language,
+                                dropdownColor: Colors.grey,
+                                icon: Icon(Icons.language),
+                                // style: appTheme.themeData.textTheme.body1,
+                                onChanged: (value) {
+                                  sessionManager.createPreferredLanguage(value);
+                                  appLanguage.changeLanguage(value);
+                                }),
+                          ]),
+                    ),
+                    Divider(),
+                    Padding(
+                      padding: EdgeInsets.symmetric(
+                        horizontal: MediaQuery.of(context).size.width / 10,
+                      ),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        //ToDo: Add Word to App Language for arabic support
+                        children: [
+                          Text('Legal Info',
+                              style: appTheme
+                                  .themeData.primaryTextTheme.subtitle1),
+                          IconButton(
+                            icon: Icon(
+                              Icons.info,
+                              color: Colors.grey,
+                            ),
+                            onPressed: () {
+                              showDialog(
+                                context: context,
+                                builder: (context) {
+                                  return AboutDialog(
+                                    applicationName: 'Ahed',
+                                    applicationVersion: '1.0.0',
+                                    children: <Widget>[
+                                      Text(
+                                          'Animations rights reserved to Lottie'),
+                                      Text(
+                                          'Fonts rights reserved to Google Fonts'),
+                                      Text('OGIVE ©${DateTime.now().year}')
+                                    ],
+                                  );
+                                },
+                              );
+                            },
+                            tooltip: 'License',
+                          ),
+                        ],
+                      ),
+                    ),
+                    Divider(),
+                    Padding(
+                      padding: EdgeInsets.symmetric(
+                        horizontal: MediaQuery.of(context).size.width / 10,
+                      ),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        //ToDo: Add Word to App Language for arabic support
+                        children: [
+                          Text('مجتمعنا',
+                              style: appTheme
+                                  .themeData.primaryTextTheme.subtitle1),
+                          IconButton(
+                            icon: Icon(
+                              Icons.people,
+                              color: Colors.grey,
+                            ),
+                            onPressed: () => commonData
+                                .changeStep(Pages.StayInTouchScreen.index),
+                            tooltip: 'License',
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                )),
+          ],
         ),
       ),
     );
--- lib/Screens/SideScreens/stay_in_touch_screen.dart
+++ lib/Screens/SideScreens/stay_in_touch_screen.dart
@@ -1,3 +1,4 @@
+import 'package:ahed/Custom%20Widgets/CustomSpacing.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_icons/flutter_icons.dart';
 import 'package:provider/provider.dart';
@@ -8,6 +9,7 @@ import '../../Shared Data/app_theme.dart';
 import '../../Shared Data/common_data.dart';
 
 class StayInTouchScreen extends StatelessWidget {
+  double w, h;
   final SessionManager sessionManager = new SessionManager();
   static CommonData commonData;
   static AppTheme appTheme;
@@ -17,84 +19,134 @@ class StayInTouchScreen extends StatelessWidget {
     commonData = Provider.of<CommonData>(context);
     appTheme = Provider.of<AppTheme>(context);
     appLanguage = Provider.of<AppLanguage>(context);
-    return
-       Container(
-        padding: EdgeInsets.only(left: 5, right: 5, top: 10, bottom: 10),
-        height: double.infinity,
-        decoration: BoxDecoration(color: appTheme.themeData.primaryColor),
-        child: SingleChildScrollView(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              // SizedBox(
-              //     width: MediaQuery.of(context).size.width,
-              //     height: NativeBannerAdSize.HEIGHT_50.height.toDouble(),
-              //     child: FacebookNativeAd(
-              //       placementId: "666800453940153_669631920323673",
-              //       adType: NativeAdType.NATIVE_BANNER_AD,
-              //       bannerAdSize: NativeBannerAdSize.HEIGHT_50,
-              //       width: double.infinity,
-              //       backgroundColor: appTheme.themeData.backgroundColor,
-              //       titleColor: appTheme.themeData.accentColor,
-              //       descriptionColor: appTheme.themeData.accentColor,
-              //       buttonColor: Colors.deepPurple,
-              //       buttonTitleColor: appTheme.themeData.accentColor,
-              //       buttonBorderColor: appTheme.themeData.accentColor,
-              //       keepAlive: true,
-              //       labelColor: Colors.transparent,
-              //       listener: (result, value) {
-              //         print("Native Ad: $result --> $value");
-              //       },
-              //     )),
-              SizedBox(
-                height: 10,
-              ),
-              CustomCard(
-                  title: appLanguage.words['StayInTouchFacebookTitle'],
-                  subtitle: appLanguage.words['StayInTouchFacebookSubtitle'],
-                  url: 'https://www.facebook.com/ogive23/',
-                  icon: Entypo.facebook,
-                  iconColor: Colors.blue,
-                  kind: 'fb',
-                  textDirection: appLanguage.textDirection),
-              SizedBox(
-                height: 20,
-              ),
-              CustomCard(
-                  title: appLanguage.words['StayInTouchInstagramTitle'],
-                  subtitle: appLanguage.words['StayInTouchInstagramSubtitle'],
-                  url: 'https://www.instagram.com/mahmoued.martin/',
-                  icon: MaterialCommunityIcons.instagram,
-                  iconColor: Colors.black,
-                  kind: 'insta',
-                  textDirection: appLanguage.textDirection),
-              SizedBox(
-                height: 20,
-              ),
-              CustomCard(
-                  title: appLanguage.words['StayInTouchYoutubeTitle'],
-                  subtitle: appLanguage.words['StayInTouchYoutubeSubtitle'],
-                  url:
-                      'https://www.youtube.com/channel/UCedueKqOIz38zog0alc7_eg',
-                  icon:Entypo.youtube,
-                  iconColor: Colors.red,
-                  kind: 'youtube',
-                  textDirection: appLanguage.textDirection),
+    w = MediaQuery.of(context).size.width;
+    h = MediaQuery.of(context).size.height;
+    return Material(
+        child: SafeArea(
+            child: Container(
+                height: double.infinity,
+                decoration: BoxDecoration(color: Colors.white),
+                child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Row(
+                        crossAxisAlignment: CrossAxisAlignment.end,
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: [
+                          Directionality(
+                            textDirection: TextDirection.ltr,
+                            child: IconButton(
+                              icon: Icon(
+                                Icons.arrow_back_ios_sharp,
+                                color: Colors.black,
+                              ),
+                              onPressed: () => commonData.back(),
+                            ),
+                          ),
+                        ],
+                      ),
+                      CustomSpacing(),
+                      Padding(
+                        padding: EdgeInsets.only(right: w / 20),
+                        child: Text(
+                          'مجتمعنا',
+                          style: appTheme.nonStaticGetTextStyle(
+                              1.0,
+                              Colors.black,
+                              appTheme.getTextTheme(context) * 1.5,
+                              FontWeight.w600,
+                              1.0,
+                              TextDecoration.none,
+                              'Delius'),
+                        ),
+                      ),
+                      CustomSpacing(),
+                      Container(
+                        padding: EdgeInsets.symmetric(
+                            vertical: h / 100, horizontal: w / 50),
+                        //   height: double.infinity,
+                        decoration: BoxDecoration(
+                            color: appTheme.themeData.primaryColor),
+                        child: SingleChildScrollView(
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            children: [
+                              //         // SizedBox(
+                              //         //     width: MediaQuery.of(context).size.width,
+                              //         //     height: NativeBannerAdSize.HEIGHT_50.height.toDouble(),
+                              //         //     child: FacebookNativeAd(
+                              //         //       placementId: "666800453940153_669631920323673",
+                              //         //       adType: NativeAdType.NATIVE_BANNER_AD,
+                              //         //       bannerAdSize: NativeBannerAdSize.HEIGHT_50,
+                              //         //       width: double.infinity,
+                              //         //       backgroundColor: appTheme.themeData.backgroundColor,
+                              //         //       titleColor: appTheme.themeData.accentColor,
+                              //         //       descriptionColor: appTheme.themeData.accentColor,
+                              //         //       buttonColor: Colors.deepPurple,
+                              //         //       buttonTitleColor: appTheme.themeData.accentColor,
+                              //         //       buttonBorderColor: appTheme.themeData.accentColor,
+                              //         //       keepAlive: true,
+                              //         //       labelColor: Colors.transparent,
+                              //         //       listener: (result, value) {
+                              //         //         print("Native Ad: $result --> $value");
+                              //         //       },
+                              //         //     )),
+                              CustomCard(
+                                  title: appLanguage
+                                      .words['StayInTouchFacebookTitle'],
+                                  subtitle: appLanguage
+                                      .words['StayInTouchFacebookSubtitle'],
+                                  url: 'https://www.facebook.com/ogive23/',
+                                  icon: Entypo.facebook,
+                                  iconColor: Colors.blue,
+                                  kind: 'fb',
+                                  textDirection: appLanguage.textDirection),
+                              SizedBox(
+                                height: h / 100,
+                              ),
+                              CustomCard(
+                                  title: appLanguage
+                                      .words['StayInTouchInstagramTitle'],
+                                  subtitle: appLanguage
+                                      .words['StayInTouchInstagramSubtitle'],
+                                  url:
+                                      'https://www.instagram.com/mahmoued.martin/',
+                                  icon: MaterialCommunityIcons.instagram,
+                                  iconColor: Colors.black,
+                                  kind: 'insta',
+                                  textDirection: appLanguage.textDirection),
+                              SizedBox(
+                                height: h / 100,
+                              ),
+                              CustomCard(
+                                  title: appLanguage
+                                      .words['StayInTouchYoutubeTitle'],
+                                  subtitle: appLanguage
+                                      .words['StayInTouchYoutubeSubtitle'],
+                                  url:
+                                      'https://www.youtube.com/channel/UCedueKqOIz38zog0alc7_eg',
+                                  icon: Entypo.youtube,
+                                  iconColor: Colors.red,
+                                  kind: 'youtube',
+                                  textDirection: appLanguage.textDirection),
 
-              SizedBox(
-                height: 20,
-              ),
-              CustomCard(
-                  title: appLanguage.words['StayInTouchTwitterTitle'],
-                  subtitle: appLanguage.words['StayInTouchTwitterSubtitle'],
-                  url: 'https://twitter.com/MahmouedMartin2',
-                  icon:Entypo.twitter,
-                  iconColor: Colors.blue,
-                  kind: 'twitter',
-                  textDirection: appLanguage.textDirection),
-            ],
-          ),
-        ),
-    );
+                              SizedBox(
+                                height: h / 100,
+                              ),
+                              CustomCard(
+                                  title: appLanguage
+                                      .words['StayInTouchTwitterTitle'],
+                                  subtitle: appLanguage
+                                      .words['StayInTouchTwitterSubtitle'],
+                                  url: 'https://twitter.com/MahmouedMartin2',
+                                  icon: Entypo.twitter,
+                                  iconColor: Colors.blue,
+                                  kind: 'twitter',
+                                  textDirection: appLanguage.textDirection),
+                            ],
+                          ),
+                        ),
+                      )
+                    ]))));
   }
 }
