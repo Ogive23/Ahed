98fb341cf7be97cac1027181f67bf8dcd11f12a6
Mahmoued Mohamed <mahmouedmartin222@yahoo.com>
2021-06-20 05:24:26 +0200

    Added Pagination support

--- lib/ApiCallers/NeedyApiCaller.dart
+++ lib/ApiCallers/NeedyApiCaller.dart
@@ -16,10 +16,31 @@ class NeedyApiCaller {
   FirebaseFirestore firestore = FirebaseFirestore.instance;
   CollectionReference urls = FirebaseFirestore.instance.collection('URLs');
   String url = "http://192.168.1.2:8000";
-  getAllUrgent() async {
+  getAllUrgent(int pageNumber) async {
     // QuerySnapshot snapshot = await urls.get();
     // for(int index = 0; index < snapshot.size; index++){
     //   String url = snapshot.docs[index]['url'];
+    // if (sessionManager.accessTokenExpired()) {
+    //   await tokenApiCaller.refreshAccessToken(sessionManager.user.id,sessionManager.oauthToken);
+    // }
+    var headers = {
+      "Content-Type": "application/json",
+      // 'Authorization': 'Bearer ${sessionManager.oauthToken}',
+    };
+    try {
+      var response = await http
+          .get(Uri.parse(url + "/api/ahed/urgentneedies?page=$pageNumber"),
+              headers: headers)
+          .catchError((error) {
+        throw responseHandler.errorPrinter(
+            "Kindly check your internet connection then try again.");
+      }).timeout(Duration(seconds: 120));
+      return {
+        "Err_Flag": false,
+        "Values": dataMapper.getNeediesFromJson(
+            url , jsonDecode(response.body)['data']['data']),
+        "total": jsonDecode(response.body)['data']['total'],
+        "lastPage": jsonDecode(response.body)['data']['last_page']
       };
     } on TimeoutException {
       return responseHandler.timeOutPrinter();
@@ -29,58 +50,112 @@ class NeedyApiCaller {
     }
     // }
   }
-  getAll() async {
+
+  getAll(int pageNumber) async {
     // QuerySnapshot snapshot = await urls.get();
     // for(int index = 0; index < snapshot.size; index++){
     //   String url = snapshot.docs[index]['url'];
-      // if (sessionManager.accessTokenExpired()) {
-      //   await tokenApiCaller.refreshAccessToken(sessionManager.user.id,sessionManager.oauthToken);
-      // }
-      var headers = {
-        "Content-Type": "application/json",
-        // 'Authorization': 'Bearer ${sessionManager.oauthToken}',
+    // if (sessionManager.accessTokenExpired()) {
+    //   await tokenApiCaller.refreshAccessToken(sessionManager.user.id,sessionManager.oauthToken);
+    // }
+    var headers = {
+      "Content-Type": "application/json",
+      // 'Authorization': 'Bearer ${sessionManager.oauthToken}',
+    };
+    try {
+      var response = await http
+          .get(Uri.parse(url + "/api/ahed/needies?page=$pageNumber"),
+              headers: headers)
+          .catchError((error) {
+        throw responseHandler.errorPrinter(
+            "Kindly check your internet connection then try again.");
+      }).timeout(Duration(seconds: 120));
+      return {
+        "Err_Flag": false,
+        "Values": dataMapper.getNeediesFromJson(
+            url , jsonDecode(response.body)['data']['data']),
+        "total": jsonDecode(response.body)['data']['total'],
+        "lastPage": jsonDecode(response.body)['data']['last_page']
       };
-      print('url = $url');
-      try {
-        var response = await http.get(Uri.parse(url+"/api/ahed/needies"), headers: headers).catchError((error) {
-          return responseHandler.errorPrinter("networkError");
-        }).timeout(Duration(seconds: 120));
-        print(response.body);
-        //ToDo:move this "/storage/" to backend and make it full link
-        return {"Err_Flag": false, "Values": dataMapper.getNeediesFromJson(url+"/storage/", jsonDecode(response.body)['data']['data'])};
-      } on TimeoutException {
-        return responseHandler.timeOutPrinter();
-      } catch (e) {
-        print(e);
-        return responseHandler.errorPrinter("networkError");
-      }
+    } on TimeoutException {
+      return responseHandler.timeOutPrinter();
+    } catch (e) {
+      print(e);
+      return e;
+    }
+    // }
+  }
+
+  getAllBookmarked(List<String> bookmarkedIDs) async {
+    print('bookmarkedIDs $bookmarkedIDs');
+    // QuerySnapshot snapshot = await urls.get();
+    // for(int index = 0; index < snapshot.size; index++){
+    //   String url = snapshot.docs[index]['url'];
+    // if (sessionManager.accessTokenExpired()) {
+    //   await tokenApiCaller.refreshAccessToken(sessionManager.user.id,sessionManager.oauthToken);
+    // }
+    var headers = {
+      "Content-Type": "application/json",
+      // 'Authorization': 'Bearer ${sessionManager.oauthToken}',
+    };
+    print('url = $url');
+    String params = '';
+    for(int index=0; index < bookmarkedIDs.length; index++)
+      params+='ids[${index.toString()}]=${bookmarkedIDs[index].toString()}&';
+    print(url+'/api/ahed/neediesWithIDs?'+params);
+    try {
+      var response = await http
+          .get(Uri.parse(url + "/api/ahed/neediesWithIDs?$params"),
+          headers: headers)
+          .catchError((error) {
+        throw responseHandler.errorPrinter(
+            "Kindly check your internet connection then try again.");
+      }).timeout(Duration(seconds: 120));
+      print(jsonDecode(response.body)['data']);
+      return {
+        "Err_Flag": false,
+        "Values": dataMapper.getNeediesFromJson(
+            url, jsonDecode(response.body)['data']),
+      };
+    } on TimeoutException {
+      return responseHandler.timeOutPrinter();
+    } catch (e) {
+      print(e);
+      return e;
+    }
     // }
   }
   create() async {
     // QuerySnapshot snapshot = await urls.get();
     // for(int index = 0; index < snapshot.size; index++){
     //   String url = snapshot.docs[index]['url'];
-      // if (sessionManager.accessTokenExpired()) {
-      //   await tokenApiCaller.refreshAccessToken(sessionManager.user.id,sessionManager.oauthToken);
-      // }
-      var headers = {
-        "Content-Type": "application/json",
-        // 'Authorization': 'Bearer ${sessionManager.oauthToken}',
-      };
-      print('url = $url');
-      try {
-        var response = await http.get(Uri.parse(url+"/api/ahed/needies"), headers: headers).catchError((error) {
-          return responseHandler.errorPrinter("networkError");
-        }).timeout(Duration(seconds: 120));
-        print(response.body);
-        //ToDo:move this "/storage/" to backend and make it full link
-        return {"Err_Flag": false, "Values": dataMapper.getNeediesFromJson(url+"/storage/", jsonDecode(response.body)['data']['data'])};
-      } on TimeoutException {
-        return responseHandler.timeOutPrinter();
-      } catch (e) {
-        print(e);
+    // if (sessionManager.accessTokenExpired()) {
+    //   await tokenApiCaller.refreshAccessToken(sessionManager.user.id,sessionManager.oauthToken);
+    // }
+    var headers = {
+      "Content-Type": "application/json",
+      // 'Authorization': 'Bearer ${sessionManager.oauthToken}',
+    };
+    print('url = $url');
+    try {
+      var response = await http
+          .get(Uri.parse(url + "/api/ahed/needies"), headers: headers)
+          .catchError((error) {
         return responseHandler.errorPrinter("networkError");
-      }
+      }).timeout(Duration(seconds: 120));
+      print(response.body);
+      //ToDo:move this "/storage/" to backend and make it full link
+      return {
+        "Err_Flag": false,
+        "Values": dataMapper.getNeediesFromJson(
+            url + "/storage/", jsonDecode(response.body)['data']['data'])
+      };
+    } on TimeoutException {
+      return responseHandler.timeOutPrinter();
+    } catch (e) {
+      print(e);
+      return responseHandler.errorPrinter("networkError");
+    }
     // }
   }
 }
