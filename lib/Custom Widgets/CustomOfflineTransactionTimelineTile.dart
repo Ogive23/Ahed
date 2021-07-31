0a71efa05619b23107c3e112634a3a3e55b7fe0a
Mahmoued Mohamed <mahmouedmartin222@yahoo.com>
2021-07-31 20:01:48 +0200

    Update OfflineTransaction info shown

--- lib/Custom Widgets/CustomOfflineTransactionTimelineTile.dart
+++ lib/Custom Widgets/CustomOfflineTransactionTimelineTile.dart
@@ -1,12 +1,17 @@
+import 'package:ahed/ApiCallers/NeedyApiCaller.dart';
 import 'package:ahed/Helpers/Helper.dart';
 import 'package:ahed/Models/OfflineTransaction.dart';
+import 'package:ahed/Screens/NeediesScreens/ShowNeedyScreen.dart';
+import 'package:ahed/Shared%20Data/app_theme.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:intl/intl.dart';
+import 'package:provider/provider.dart';
 import 'package:timeline_tile/timeline_tile.dart';
 
 class CustomOfflineTransactionTimelineTile extends StatelessWidget {
   final OfflineTransaction transaction;
+  static late AppTheme appTheme;
   //ToDo: Ahed v2 prize
   final bool done;
   final Helper helper = new Helper();
@@ -14,13 +19,14 @@ class CustomOfflineTransactionTimelineTile extends StatelessWidget {
       {required this.transaction, required this.done});
   @override
   Widget build(BuildContext context) {
+    appTheme = Provider.of<AppTheme>(context);
     return TimelineTile(
       alignment: TimelineAlign.manual,
       lineXY: 0.5,
       beforeLineStyle:
           LineStyle(color: Colors.black.withOpacity(0.1), thickness: 3),
       indicatorStyle: IndicatorStyle(
-        indicatorXY: 0.3, iconStyle: IconStyle(iconData:FontAwesomeIcons.leaf),
+        indicatorXY: 0.3, iconStyle: IconStyle(iconData: FontAwesomeIcons.leaf),
         drawGap: true,
         width: 30,
         // height: 30,
@@ -37,14 +43,14 @@ class CustomOfflineTransactionTimelineTile extends StatelessWidget {
         child: Container(
           alignment: const Alignment(0.0, -0.50),
           child: Text(
-                  helper.getAppropriateText(
-                      DateFormat('y/MM/dd').format(transaction.createdAt)),
-                  style: TextStyle(
-                    fontSize: 16,
-                    color: Colors.black.withOpacity(0.6),
-                    fontWeight: FontWeight.w800,
-                  ),
-                ),
+            helper.getAppropriateText(
+                DateFormat('y/MM/dd').format(transaction.createdAt)),
+            style: TextStyle(
+              fontSize: 16,
+              color: Colors.black.withOpacity(0.6),
+              fontWeight: FontWeight.w800,
+            ),
+          ),
         ),
       ),
       endChild: Padding(
@@ -61,23 +67,76 @@ class CustomOfflineTransactionTimelineTile extends StatelessWidget {
                 fontWeight: FontWeight.bold,
               ),
             ),
-            Text(
-              'تبرعك ذهب إلي جهة ${helper.getAppropriateText(transaction.preferredSection)}',
-              style: TextStyle(
-                fontSize: 14,
-                color: Colors.black.withOpacity(0.6),
-                fontWeight: FontWeight.normal,
-              ),
-            ),
-            Text(
-                transaction.selectedDate == null? 'سيتم التواصل معكم قريباً':'سيصل مندوبنا يوم ${helper.getAppropriateText(
-                  DateFormat('y/MM/dd').format(transaction.selectedDate!))}',
-              style: TextStyle(
-                fontSize: 14,
-                color: Colors.black.withOpacity(0.6),
-                fontWeight: FontWeight.normal,
-              ),
-            )
+            transaction.needy == null
+                ? Text(
+                    'تبرعك ذهب إلي جهة ${helper.getAppropriateText(transaction.preferredSection)}',
+                    style: TextStyle(
+                      fontSize: 14,
+                      color: Colors.black.withOpacity(0.6),
+                      fontWeight: FontWeight.normal,
+                    ),
+                  )
+                : GestureDetector(
+                    onTap: () async {
+                      NeedyApiCaller needyApiCaller = new NeedyApiCaller();
+                      Map<String, dynamic> status =
+                          await needyApiCaller.getById(transaction.needy!);
+                      if (!status['Err_Flag'])
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => ShowNeedyScreen(
+                                needy: status['Value'],
+                                appTheme: appTheme,
+                              ),
+                            ));
+                    },
+                    child: Text('عرض الحالة المرتبطة')),
+            transaction.collected
+                ? SizedBox()
+                : transaction.selectedDate != null
+                    ? Text(
+                        'سيصل مندوبنا يوم ${helper.getAppropriateText(DateFormat('y/MM/dd').format(transaction.selectedDate!))}',
+                        style: TextStyle(
+                          fontSize: 14,
+                          color: Colors.black.withOpacity(0.6),
+                          fontWeight: FontWeight.normal,
+                        ),
+                      )
+                    : Column(
+                        children: [
+                          Text('سيتم التواصل معكم قريباً'),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceAround,
+                            children: [
+                              GestureDetector(
+                                onTap: () {
+                                  
+                                },
+                                child: Text(
+                                  'تعديل',
+                                  style: TextStyle(
+                                      fontSize: 14,
+                                      color: Colors.blue.withOpacity(0.6),
+                                      fontWeight: FontWeight.normal,
+                                      decoration: TextDecoration.underline),
+                                ),
+                              ),
+                              GestureDetector(
+                                onTap: () {},
+                                child: Text(
+                                  'حذف',
+                                  style: TextStyle(
+                                      fontSize: 14,
+                                      color: Colors.red.withOpacity(0.6),
+                                      fontWeight: FontWeight.normal,
+                                      decoration: TextDecoration.underline),
+                                ),
+                              ),
+                            ],
+                          )
+                        ],
+                      ),
           ],
         ),
       ),
