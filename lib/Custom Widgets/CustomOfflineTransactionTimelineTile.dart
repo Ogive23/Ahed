469e3bbd4b43e211f09055e894c7f317211eb3e0
Mahmoued Mohamed <mahmouedmartin222@yahoo.com>
2021-08-16 15:05:56 +0200

    Updated offline donations UI

--- lib/Custom Widgets/CustomOfflineTransactionTimelineTile.dart
+++ lib/Custom Widgets/CustomOfflineTransactionTimelineTile.dart
@@ -1,25 +1,129 @@
 import 'package:ahed/ApiCallers/NeedyApiCaller.dart';
+import 'package:ahed/ApiCallers/TransactionApiCaller.dart';
+import 'package:ahed/Custom%20Widgets/CustomSpacing.dart';
 import 'package:ahed/Helpers/Helper.dart';
+import 'package:ahed/Models/Needy.dart';
 import 'package:ahed/Models/OfflineTransaction.dart';
-import 'package:ahed/Screens/NeediesScreens/ShowNeedyScreen.dart';
+import 'package:ahed/Session/session_manager.dart';
+import 'package:ahed/Shared%20Data/NeedyData.dart';
+import 'package:ahed/Shared%20Data/TransactionData.dart';
 import 'package:ahed/Shared%20Data/app_theme.dart';
+import 'package:ahed/Shared%20Data/common_data.dart';
+import 'package:cool_alert/cool_alert.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'package:intl/intl.dart';
+import 'package:intl/intl.dart' as intl;
 import 'package:provider/provider.dart';
 import 'package:timeline_tile/timeline_tile.dart';
+import '../GeneralInfo.dart';
+import 'package:ahed/Custom Widgets/NeedyInfoDialog.dart';
 
 class CustomOfflineTransactionTimelineTile extends StatelessWidget {
   final OfflineTransaction transaction;
   static late AppTheme appTheme;
+  static late CommonData commonData;
+  static late NeedyData needyData;
+  static late TransactionData transactionData;
   //ToDo: Ahed v2 prize
   final bool done;
   final Helper helper = new Helper();
+  static late double w, h;
+  Function callBack;
+
   CustomOfflineTransactionTimelineTile(
-      {required this.transaction, required this.done});
+      {required this.transaction, required this.done, required this.callBack});
+
+  showNeedyDialog(context, Needy needy) async {
+    return showDialog<void>(
+      context: context,
+      barrierDismissible: true,
+      builder: (BuildContext dialogContext) {
+        return NeedyInfoDialog(needy: needy,appTheme: appTheme,);
+      },
+    );
+  }
+
+  onDeleteSelected(context) {
+    return showDialog<void>(
+      context: context,
+      barrierDismissible: true,
+      builder: (BuildContext dialogContext) {
+        return Directionality(
+          textDirection: TextDirection.rtl,
+          child: AlertDialog(
+            backgroundColor: appTheme.themeData.cardColor,
+            titlePadding:
+                EdgeInsets.symmetric(horizontal: w / 50, vertical: h / 50),
+            contentPadding:
+                EdgeInsets.symmetric(horizontal: w / 25, vertical: h / 50),
+            title: Text(
+              'هل تريد حذف هذا التبرع؟',
+              style: appTheme.themeData.primaryTextTheme.headline5,
+            ),
+            actions: <Widget>[
+              FlatButton(
+                child: Text(
+                  'نعم',
+                  style: TextStyle(color: Colors.white),
+                ),
+                shape: Border.all(color: Colors.white),
+                color: Colors.green,
+                onPressed: () async {
+                  TransactionApiCaller transactionApiCaller =
+                      new TransactionApiCaller();
+                  SessionManager sessionManager = new SessionManager();
+                  Map<String, dynamic> status =
+                      await transactionApiCaller.deleteOfflineTransactions(
+                          sessionManager.user!.id, transaction.id);
+                  Navigator.of(dialogContext).pop();
+                  if (status['Err_Flag']) {
+                    return CoolAlert.show(
+                        context: context,
+                        type: CoolAlertType.error,
+                        lottieAsset: 'assets/animations/38213-error.json',
+                        text: status['Err_Desc'],
+                        confirmBtnColor: Color(0xff1c9691),
+                        title: '');
+                  } else {
+                    // commonData.back();
+                    callBack();
+                    return CoolAlert.show(
+                        context: context,
+                        type: CoolAlertType.success,
+                        lottieAsset: 'assets/animations/6951-success.json',
+                        text: status['message'],
+                        confirmBtnColor: Color(0xff1c9691),
+                        title: '');
+                  }
+                },
+              ),
+              FlatButton(
+                child: Text(
+                  'لا',
+                  style: TextStyle(color: Colors.white),
+                ),
+                shape: Border.all(color: Colors.white),
+                color: Colors.red,
+                onPressed: () {
+                  Navigator.of(dialogContext).pop();
+                },
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
+    w = MediaQuery.of(context).size.width;
+    h = MediaQuery.of(context).size.height;
     appTheme = Provider.of<AppTheme>(context);
+    commonData = Provider.of<CommonData>(context);
+    needyData = Provider.of<NeedyData>(context);
+    transactionData = Provider.of<TransactionData>(context);
+
     return TimelineTile(
       alignment: TimelineAlign.manual,
       lineXY: 0.5,
@@ -44,12 +148,8 @@ class CustomOfflineTransactionTimelineTile extends StatelessWidget {
           alignment: const Alignment(0.0, -0.50),
           child: Text(
             helper.getAppropriateText(
-                DateFormat('y/MM/dd').format(transaction.createdAt)),
-            style: TextStyle(
-              fontSize: 16,
-              color: Colors.black.withOpacity(0.6),
-              fontWeight: FontWeight.w800,
-            ),
+                intl.DateFormat('y/MM/dd').format(transaction.createdAt)),
+            style: appTheme.themeData.primaryTextTheme.headline4,
           ),
         ),
       ),
@@ -61,77 +161,102 @@ class CustomOfflineTransactionTimelineTile extends StatelessWidget {
           children: <Widget>[
             Text(
               transaction.amount.toStringAsFixed(2) + " جنيه مصري",
-              style: TextStyle(
-                fontSize: 18,
-                color: Colors.black.withOpacity(0.8),
-                fontWeight: FontWeight.bold,
-              ),
+              style: appTheme.themeData.primaryTextTheme.headline5!
+                  .apply(fontWeightDelta: 2),
             ),
+            CustomSpacing(value: 100),
             transaction.needy == null
                 ? Text(
                     'تبرعك ذهب إلي جهة ${helper.getAppropriateText(transaction.preferredSection)}',
-                    style: TextStyle(
-                      fontSize: 14,
-                      color: Colors.black.withOpacity(0.6),
-                      fontWeight: FontWeight.normal,
-                    ),
+                    style: appTheme.themeData.primaryTextTheme.headline5,
                   )
                 : GestureDetector(
                     onTap: () async {
+                      CoolAlert.show(
+                          context: context,
+                          type: CoolAlertType.loading,
+                          lottieAsset: 'assets/animations/890-loading-animation.json',
+                          text: 'جاري تحميل بيانات الحالة',
+                          // confirmBtnColor: Color(0xff1c9691),
+                          title: '');
                       NeedyApiCaller needyApiCaller = new NeedyApiCaller();
                       Map<String, dynamic> status =
                           await needyApiCaller.getById(transaction.needy!);
-                      if (!status['Err_Flag'])
-                        Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) => ShowNeedyScreen(
-                                needy: status['Value'],
-                                appTheme: appTheme,
-                              ),
-                            ));
+                      Navigator.pop(context);
+                      if (!status['Err_Flag']) {
+                        showNeedyDialog(context, status['Value']);
+                      } else {
+                        return CoolAlert.show(
+                            context: context,
+                            type: CoolAlertType.error,
+                            lottieAsset:
+                            'assets/animations/38213-error.json',
+                            text: status['Err_Desc'],
+                            confirmBtnColor: Color(0xff1c9691),
+                            title: '');
+                      }
+
                     },
-                    child: Text('عرض الحالة المرتبطة')),
+                    child: Text(
+                      'عرض الحالة المرتبطة',
+                      style: appTheme.themeData.primaryTextTheme.headline5!
+                          .apply(
+                              decoration: TextDecoration.underline,
+                              color: Colors.blue),
+                    )),
+            CustomSpacing(value: 100),
             transaction.collected
                 ? SizedBox()
                 : transaction.selectedDate != null
                     ? Text(
-                        'سيصل مندوبنا يوم ${helper.getAppropriateText(DateFormat('y/MM/dd').format(transaction.selectedDate!))}',
-                        style: TextStyle(
-                          fontSize: 14,
-                          color: Colors.black.withOpacity(0.6),
-                          fontWeight: FontWeight.normal,
-                        ),
-                      )
+                        'سيصل مندوبنا يوم ${helper.getAppropriateText(intl.DateFormat('y/MM/dd').format(transaction.selectedDate!))}',
+                        style: appTheme.themeData.primaryTextTheme.headline5)
                     : Column(
                         children: [
-                          Text('سيتم التواصل معكم قريباً'),
+                          Text('سيتم التواصل معكم قريباً',
+                              style: appTheme
+                                  .themeData.primaryTextTheme.headline5),
+                          CustomSpacing(value: 100),
                           Row(
                             mainAxisAlignment: MainAxisAlignment.spaceAround,
                             children: [
                               GestureDetector(
-                                onTap: () {
-                                  
+                                onTap: () async {
+                                  NeedyApiCaller needyApiCaller =
+                                      new NeedyApiCaller();
+                                  Map<String, dynamic> status =
+                                      await needyApiCaller
+                                          .getById(transaction.needy!);
+                                  if (!status['Err_Flag']) {
+                                    needyData.chooseNeedy(status['Value']);
+                                    transactionData
+                                        .chooseTransaction(transaction);
+                                    commonData.changeStep(Pages
+                                        .OfflineTransactionUpdateScreen.index);
+                                  } else {
+                                    return CoolAlert.show(
+                                        context: context,
+                                        type: CoolAlertType.error,
+                                        lottieAsset:
+                                            'assets/animations/38213-error.json',
+                                        text: status['Err_Desc'],
+                                        confirmBtnColor: Color(0xff1c9691),
+                                        title: '');
+                                  }
                                 },
-                                child: Text(
-                                  'تعديل',
-                                  style: TextStyle(
-                                      fontSize: 14,
-                                      color: Colors.blue.withOpacity(0.6),
-                                      fontWeight: FontWeight.normal,
-                                      decoration: TextDecoration.underline),
-                                ),
+                                child: Text('تعديل',
+                                    style: appTheme
+                                        .themeData.primaryTextTheme.headline5!
+                                        .apply(color: Colors.blue)),
                               ),
                               GestureDetector(
-                                onTap: () {},
-                                child: Text(
-                                  'حذف',
-                                  style: TextStyle(
-                                      fontSize: 14,
-                                      color: Colors.red.withOpacity(0.6),
-                                      fontWeight: FontWeight.normal,
-                                      decoration: TextDecoration.underline),
-                                ),
+                                onTap: () async {
+                                  await onDeleteSelected(context);
+                                },
+                                child: Text('حذف',
+                                    style: appTheme
+                                        .themeData.primaryTextTheme.headline5!
+                                        .apply(color: Colors.red)),
                               ),
                             ],
                           )
---
+++ lib/Custom Widgets/NeedyInfoDialog.dart
@@ -0,0 +1,227 @@
+import 'package:ahed/Models/Needy.dart';
+import 'package:ahed/Shared%20Data/app_theme.dart';
+import 'package:carousel_slider/carousel_options.dart';
+import 'package:carousel_slider/carousel_slider.dart';
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:percent_indicator/linear_percent_indicator.dart';
+import 'package:provider/provider.dart';
+import 'package:share/share.dart';
+
+import 'CustomSpacing.dart';
+import 'ImageCarouselShow.dart';
+
+class NeedyInfoDialog extends StatelessWidget {
+  final Needy needy;
+  final AppTheme appTheme;
+  int currentIndex = 0;
+  static late double w, h;
+  NeedyInfoDialog({required this.needy, required this.appTheme});
+
+  @override
+  Widget build(BuildContext context) {
+    w = MediaQuery.of(context).size.width;
+    h = MediaQuery.of(context).size.height;
+    return Directionality(
+      textDirection: TextDirection.rtl,
+      child: AlertDialog(
+        backgroundColor: appTheme.themeData.cardColor,
+        scrollable: true,
+        contentPadding:
+            EdgeInsets.symmetric(horizontal: w / 50, vertical: h / 50),
+        content: Container(
+            width: w,
+            child: SingleChildScrollView(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Padding(
+                      padding: EdgeInsets.only(top: h / 50),
+                      child: CarouselSlider(
+                        options: CarouselOptions(
+                            // height: h / 3,
+                            onPageChanged: (index, reason) =>
+                                currentIndex = index,
+                            enlargeCenterPage: true,
+                            scrollDirection: Axis.horizontal,
+                            enableInfiniteScroll: true,
+                            viewportFraction: 0.8),
+                        items: needy.imagesBefore!
+                            .map((image) => GestureDetector(
+                                  onTap: () {
+                                    Navigator.push(
+                                        context,
+                                        MaterialPageRoute(
+                                            builder: (context) =>
+                                                ImageCarouselShow(
+                                                  needyMedias:
+                                                      needy.imagesBefore!,
+                                                  currentIndex: currentIndex,
+                                                  type: 'Before',
+                                                  appTheme: appTheme,
+                                                )));
+                                  },
+                                  child: Image.network(
+                                    image.url,
+                                    fit: BoxFit.cover,
+                                    height: h / 3,
+                                    width: w,
+                                  ),
+                                ))
+                            .toList(),
+                      )),
+                  Align(
+                    alignment: Alignment.topLeft,
+                    child: Visibility(
+                      visible: needy.severity! >= 7,
+                      child: Container(
+                        padding: EdgeInsets.symmetric(
+                            vertical: h / 100, horizontal: w / 20),
+                        decoration: BoxDecoration(
+                          color: Colors.red[500],
+                        ),
+                        child: Text(
+                          'حالة حرجة',
+                          style: appTheme.themeData.primaryTextTheme.headline5!
+                              .apply(color: Colors.white),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Container(
+                    width: w,
+                    child: Column(
+                      children: [
+                        Row(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text('نوع الحالة: ',
+                                style: appTheme
+                                    .themeData.primaryTextTheme.headline5),
+                            Flexible(
+                              child: Text('${needy.type}',
+                                  style: appTheme
+                                      .themeData.primaryTextTheme.headline5),
+                            ),
+                          ],
+                        ),
+                        CustomSpacing(
+                          value: 100,
+                        ),
+                        Container(
+                          height: h / 4,
+                          child: SingleChildScrollView(
+                            child: Row(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text('التفاصيل: ',
+                                    style: appTheme
+                                        .themeData.primaryTextTheme.headline5),
+                                Flexible(
+                                  child: Text(
+                                    '${needy.details}',
+                                    style: appTheme
+                                        .themeData.primaryTextTheme.headline5,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                        CustomSpacing(
+                          value: 100,
+                        ),
+                        Row(
+                          children: [
+                            Text('مسار النجاح: ',
+                                style: appTheme
+                                    .themeData.primaryTextTheme.headline5),
+                            LinearPercentIndicator(
+                              width: w / 2,
+                              alignment: MainAxisAlignment.center,
+                              animation: true,
+                              padding: EdgeInsets.symmetric(vertical: h / 100),
+                              animationDuration: 1000,
+                              lineHeight: h / 25,
+                              linearGradient: LinearGradient(
+                                  colors: [Colors.green, Colors.greenAccent]),
+                              percent: needy.collected! / needy.need!,
+                              center: Text(
+                                (needy.need! - needy.collected!)
+                                        .toStringAsFixed(0) +
+                                    ' جنيه متبقي',
+                                style: appTheme
+                                    .themeData.primaryTextTheme.bodyText2,
+                              ),
+                              linearStrokeCap: LinearStrokeCap.butt,
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                  CustomSpacing(
+                    value: 100,
+                  ),
+                  needy.satisfied!
+                      ? Center(
+                          child: ElevatedButton(
+                            onPressed: () {
+                              Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) => ImageCarouselShow(
+                                            needyMedias: needy.imagesAfter!,
+                                            type: 'After',
+                                            currentIndex: currentIndex,
+                                            appTheme: appTheme,
+                                          )));
+                            },
+                            child: Text(
+                              'أظهر التغيير',
+                              style:
+                                  appTheme.themeData.primaryTextTheme.bodyText2,
+                            ),
+                            style: ButtonStyle(
+                                backgroundColor:
+                                    MaterialStateProperty.all<Color>(
+                                        Colors.green[500]!.withOpacity(0.7))),
+                          ),
+                        )
+                      : Center(
+                          child: ElevatedButton(
+                            child: Row(
+                              mainAxisSize: MainAxisSize.min,
+                              children: [
+                                Text('مشاركة',
+                                    style: appTheme
+                                        .themeData.primaryTextTheme.bodyText2),
+                                Icon(
+                                  // Icons.share,
+                                  FontAwesomeIcons.share,
+                                  color: Colors.white,
+                                  size: appTheme.getTextTheme(context),
+                                ),
+                              ],
+                            ),
+                            onPressed: () {
+                              Share.share(
+                                //ToDo: Add application url
+                                needy.satisfied!
+                                    ? 'Check out what Ahed made to change these people\'s life ${needy.url!}, You can start to be part of it by downloading Ahed Application from ${'Application URL'}.'
+                                    : 'Only ${needy.need! - needy.collected!} EGP Left, Help ${needy.name!} To ${needy.type!}!\n ${needy.url!}',
+                              );
+                            },
+                            style: ButtonStyle(
+                                backgroundColor:
+                                    MaterialStateProperty.all<Color>(
+                                        Colors.blue[400]!)),
+                          ),
+                        ),
+                ],
+              ),
+            )),
+      ),
+    );
+  }
+}
