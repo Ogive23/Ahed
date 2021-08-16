e509a4f2ceb7feced47aa8fca959f587c53e8e57
Mahmoued Mohamed <mahmouedmartin222@yahoo.com>
2021-08-16 15:06:10 +0200

    Updated online donations UI

--- lib/Custom Widgets/CustomOnlineTransactionTimelineTile.dart
+++ lib/Custom Widgets/CustomOnlineTransactionTimelineTile.dart
@@ -1,13 +1,16 @@
 import 'package:ahed/ApiCallers/NeedyApiCaller.dart';
 import 'package:ahed/Helpers/Helper.dart';
+import 'package:ahed/Models/Needy.dart';
 import 'package:ahed/Models/OnlineTransaction.dart';
-import 'package:ahed/Screens/NeediesScreens/ShowNeedyScreen.dart';
 import 'package:ahed/Shared%20Data/app_theme.dart';
+import 'package:cool_alert/cool_alert.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'package:intl/intl.dart';
+import 'package:intl/intl.dart' as intl;
 import 'package:provider/provider.dart';
 import 'package:timeline_tile/timeline_tile.dart';
+import 'CustomSpacing.dart';
+import 'package:ahed/Custom Widgets/NeedyInfoDialog.dart';
 
 class CustomOnlineTransactionTimelineTile extends StatelessWidget {
   final OnlineTransaction transaction;
@@ -15,10 +18,28 @@ class CustomOnlineTransactionTimelineTile extends StatelessWidget {
   //ToDo: Ahed v2 prize
   final bool done;
   final Helper helper = new Helper();
+  static late double w, h;
+
   CustomOnlineTransactionTimelineTile(
       {required this.transaction, required this.done});
+
+  showNeedyDialog(context, Needy needy) async {
+    return showDialog<void>(
+      context: context,
+      barrierDismissible: true,
+      builder: (BuildContext dialogContext) {
+        return NeedyInfoDialog(
+          needy: needy,
+          appTheme: appTheme,
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
+    w = MediaQuery.of(context).size.width;
+    h = MediaQuery.of(context).size.height;
     appTheme = Provider.of<AppTheme>(context);
     return TimelineTile(
       alignment: TimelineAlign.manual,
@@ -44,12 +65,8 @@ class CustomOnlineTransactionTimelineTile extends StatelessWidget {
           alignment: const Alignment(0.0, -0.50),
           child: Text(
             helper.getAppropriateText(
-                DateFormat('y/MM/dd').format(transaction.createdAt)),
-            style: TextStyle(
-              fontSize: 16,
-              color: Colors.black.withOpacity(0.6),
-              fontWeight: FontWeight.w800,
-            ),
+                intl.DateFormat('y/MM/dd').format(transaction.createdAt)),
+            style: appTheme.themeData.primaryTextTheme.headline4,
           ),
         ),
       ),
@@ -61,31 +78,45 @@ class CustomOnlineTransactionTimelineTile extends StatelessWidget {
           children: <Widget>[
             Text(
               transaction.amount.toStringAsFixed(2) + " جنيه مصري",
-              style: TextStyle(
-                fontSize: 18,
-                color: Colors.black.withOpacity(0.8),
-                fontWeight: FontWeight.bold,
-              ),
+              style: appTheme.themeData.primaryTextTheme.headline5!
+                  .apply(fontWeightDelta: 2),
             ),
+            CustomSpacing(value: 100),
             transaction.needy == null
                 ? Text('لا حالة مرتبطة.')
                 : GestureDetector(
                     onTap: () async {
+                      CoolAlert.show(
+                          context: context,
+                          type: CoolAlertType.loading,
+                          lottieAsset:
+                              'assets/animations/890-loading-animation.json',
+                          text: 'جاري تحميل بيانات الحالة',
+                          // confirmBtnColor: Color(0xff1c9691),
+                          title: '');
                       NeedyApiCaller needyApiCaller = new NeedyApiCaller();
                       Map<String, dynamic> status =
                           await needyApiCaller.getById(transaction.needy!);
-                      if (!status['Err_Flag'])
-                        Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) => ShowNeedyScreen(
-                                needy: status['Value'],
-                                appTheme: appTheme,
-
-                              ),
-                            ));
+                      Navigator.pop(context);
+                      if (!status['Err_Flag']) {
+                        showNeedyDialog(context, status['Value']);
+                      } else {
+                        return CoolAlert.show(
+                            context: context,
+                            type: CoolAlertType.error,
+                            lottieAsset: 'assets/animations/38213-error.json',
+                            text: status['Err_Desc'],
+                            confirmBtnColor: Color(0xff1c9691),
+                            title: '');
+                      }
                     },
-                    child: Text('عرض الحالة')),
+                    child: Text(
+                      'عرض الحالة المرتبطة',
+                      style: appTheme.themeData.primaryTextTheme.headline5!
+                          .apply(
+                              decoration: TextDecoration.underline,
+                              color: Colors.blue),
+                    )),
           ],
         ),
       ),
